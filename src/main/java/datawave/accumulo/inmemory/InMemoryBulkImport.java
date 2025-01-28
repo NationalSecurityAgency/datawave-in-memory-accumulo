24ac5971f29943cdbdc707829faf36f579a3f338
Daniel Roberts ddanielr <ddanielr@gmail.com>
2025-01-28 00:14:38 +0000

    Fix version numbers and bulkImport method
    Use datawave instead of SNAPSHOT.
    Fixes the bulkImport method so that tests continue to pass

--- src/main/java/datawave/accumulo/inmemory/InMemoryBulkImport.java
+++ src/main/java/datawave/accumulo/inmemory/InMemoryBulkImport.java
@@ -1,16 +1,16 @@
 package datawave.accumulo.inmemory;
 
-import com.google.common.base.Preconditions;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.concurrent.Executor;
+
 import org.apache.accumulo.core.client.AccumuloException;
 import org.apache.accumulo.core.client.AccumuloSecurityException;
 import org.apache.accumulo.core.client.TableNotFoundException;
 import org.apache.accumulo.core.client.admin.TableOperations.ImportDestinationArguments;
 import org.apache.accumulo.core.client.admin.TableOperations.ImportMappingOptions;
-import org.apache.accumulo.core.clientImpl.AccumuloBulkMergeException;
-import org.apache.accumulo.core.clientImpl.TableOperationsImpl;
-import org.apache.accumulo.core.clientImpl.bulk.BulkSerialize;
 import org.apache.accumulo.core.conf.DefaultConfiguration;
-import org.apache.accumulo.core.conf.Property;
 import org.apache.accumulo.core.crypto.CryptoFactoryLoader;
 import org.apache.accumulo.core.data.*;
 import org.apache.accumulo.core.file.FileOperations;
@@ -19,24 +19,14 @@ import org.apache.accumulo.core.metadata.UnreferencedTabletFile;
 import org.apache.accumulo.core.security.ColumnVisibility;
 import org.apache.accumulo.core.spi.crypto.CryptoEnvironment;
 import org.apache.accumulo.core.spi.crypto.CryptoService;
-import org.apache.accumulo.core.util.Retry;
 import org.apache.accumulo.core.util.Validators;
-import org.apache.accumulo.core.volume.VolumeConfiguration;
-import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.DataInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
-import java.time.Duration;
-import java.util.*;
-import java.util.concurrent.Executor;
+import com.google.common.base.Preconditions;
 
 public class InMemoryBulkImport implements ImportDestinationArguments, ImportMappingOptions {
     private static final Logger log = LoggerFactory.getLogger(InMemoryBulkImport.class);
@@ -49,51 +39,48 @@ public class InMemoryBulkImport implements ImportDestinationArguments, ImportMap
     private int numThreads = -1;
     private String tableName;
     private LoadPlan plan = null;
-    private static final byte[] byte0 = new byte[]{0};
-
-
+    private static final byte[] byte0 = new byte[] {0};
+    
     InMemoryBulkImport(InMemoryAccumulo acu, String directory) {
         this.dir = Objects.requireNonNull(directory);
         this.acu = Objects.requireNonNull(acu);
     }
-
+    
     public ImportMappingOptions tableTime(boolean value) {
         this.setTime = value;
         return this;
     }
-
+    
     public ImportMappingOptions ignoreEmptyDir(boolean ignore) {
         this.ignoreEmptyDir = ignore;
         return this;
     }
-
-
+    
     @Override
     public ImportMappingOptions to(String tableName) {
         this.tableName = Validators.EXISTING_TABLE_NAME.validate(tableName);
         return this;
     }
-
+    
     @Override
     public ImportMappingOptions plan(LoadPlan plan) {
         this.plan = Objects.requireNonNull(plan);
         return this;
     }
-
+    
     @Override
     public ImportMappingOptions executor(Executor service) {
         this.executor = Objects.requireNonNull(service);
         return this;
     }
-
+    
     @Override
     public ImportMappingOptions threads(int numThreads) {
         Preconditions.checkArgument(numThreads > 0, "Non positive number of threads given : %s", numThreads);
         this.numThreads = numThreads;
         return this;
     }
-
-
+    
     @Override
     public void load() throws TableNotFoundException, IOException, AccumuloException, AccumuloSecurityException {
         long time = System.currentTimeMillis();
@@ -101,7 +88,7 @@ public class InMemoryBulkImport implements ImportDestinationArguments, ImportMap
         if (table == null) {
             throw new TableNotFoundException(null, tableName, "The table was not found");
         }
-
+        
         FileSystem fs = acu.getFileSystem();
         Path srcPath = this.checkPath(fs, this.dir);
         /*
@@ -111,8 +98,8 @@ public class InMemoryBulkImport implements ImportDestinationArguments, ImportMap
             try {
                 CryptoService cs = CryptoFactoryLoader.getServiceForClient(CryptoEnvironment.Scope.TABLE, table.settings);
                 FileSKVIterator importIterator = FileOperations.getInstance().newReaderBuilder()
-                        .forFile(UnreferencedTabletFile.of(fs, importStatus.getPath()), fs, fs.getConf(), cs)
-                        .withTableConfiguration(DefaultConfiguration.getInstance()).seekToBeginning().build();
+                                .forFile(UnreferencedTabletFile.of(fs, importStatus.getPath()), fs, fs.getConf(), cs)
+                                .withTableConfiguration(DefaultConfiguration.getInstance()).seekToBeginning().build();
                 while (importIterator.hasTop()) {
                     Key key = importIterator.getTopKey();
                     Value value = importIterator.getTopValue();
@@ -122,10 +109,10 @@ public class InMemoryBulkImport implements ImportDestinationArguments, ImportMap
                     Mutation mutation = new Mutation(key.getRow());
                     if (!key.isDeleted()) {
                         mutation.put(key.getColumnFamily(), key.getColumnQualifier(), new ColumnVisibility(key.getColumnVisibilityData().toArray()),
-                                key.getTimestamp(), value);
+                                        key.getTimestamp(), value);
                     } else {
                         mutation.putDelete(key.getColumnFamily(), key.getColumnQualifier(), new ColumnVisibility(key.getColumnVisibilityData().toArray()),
-                                key.getTimestamp());
+                                        key.getTimestamp());
                     }
                     table.addMutation(mutation);
                     importIterator.next();
@@ -136,10 +123,10 @@ public class InMemoryBulkImport implements ImportDestinationArguments, ImportMap
             fs.delete(importStatus.getPath(), true);
         }
     }
-
+    
     private Path checkPath(FileSystem fs, String dir) throws IOException, AccumuloException {
         Path ret = dir.contains(":") ? new Path(dir) : fs.makeQualified(new Path(dir));
-
+        
         try {
             if (!fs.getFileStatus(ret).isDirectory()) {
                 throw new AccumuloException("Bulk import directory " + dir + " is not a directory!");
